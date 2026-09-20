5f92fe6dfb67cc1e30d6063a061ccef6e412e614
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-20 16:36:50 -0400

    Fix Capital One manual sync request

--- extensions/capital-one/service-worker.js
+++ extensions/capital-one/service-worker.js
@@ -3,8 +3,11 @@ chrome.runtime.onMessage.addListener(async (msg) => {
     const tabs = await chrome.tabs.query({url:["https://jtczy6pfmy.github.io/Money-Hub/*"]});
     for (const tab of tabs) if (tab.id) chrome.tabs.sendMessage(tab.id,msg).catch(()=>{});
   }
+
   if (msg?.type === "REQUEST_CAPITAL_ONE_SYNC") {
     const tabs = await chrome.tabs.query({url:["https://*.capitalone.com/*"]});
-    for (const tab of tabs) if (tab.id) chrome.tabs.sendMessage(tab.id,{type:"CAPITAL_ONE_BALANCE",balance:undefined}).catch(()=>{});
+    for (const tab of tabs) if (tab.id) {
+      chrome.tabs.sendMessage(tab.id,{type:"REQUEST_CAPITAL_ONE_SYNC"}).catch(()=>{});
+    }
   }
 });
