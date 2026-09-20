4dab208010740b67616bb5daef40513d079595a4
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-20 16:27:30 -0400

    Remove unfinished Capital One extension changes

--- extensions/capital-one/app-bridge.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-  if (!message || message.type !== "CAPITAL_ONE_BALANCE") return;
-  window.postMessage({
-    source: "money-hub-extension",
-    type: "CAPITAL_ONE_BALANCE",
-    balance: message.balance,
-    accountName: message.accountName || "Capital One"
-  }, window.location.origin);
-});
