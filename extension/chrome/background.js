ac1d839340448fbba325ccb2ff0b7f4617b7d07e
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-20 15:37:36 -0400

    Remove extension integration to restore Money Hub login

--- extension/chrome/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
-  if (message?.type !== "MONEY_HUB_BALANCES_DETECTED") return;
-  chrome.storage.session.set({
-    lastBalances: message.balances || [],
-    lastUrl: sender.tab?.url || "",
-    lastTitle: sender.tab?.title || ""
-  });
-});
