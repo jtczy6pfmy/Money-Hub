728a6c3c1ff07b6026b4ee8ca0c96c628ed0fe0e
jtczy6pfmy <jtczy6pfmy@privaterelay.appleid.com>
2026-09-20 16:40:42 -0400

    Make Money Hub bridge cross-browser

--- extensions/capital-one/money-hub-bridge.js
+++ extensions/capital-one/money-hub-bridge.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener((msg) => {
+(typeof browser !== "undefined" ? browser : chrome).runtime.onMessage.addListener((msg) => {
   if (msg?.type === "CAPITAL_ONE_BALANCE") {
     window.postMessage({type:"MONEY_HUB_CAPITAL_ONE_BALANCE", balance:msg.balance}, window.location.origin);
   }
@@ -6,6 +6,6 @@ chrome.runtime.onMessage.addListener((msg) => {
 window.addEventListener("message", (event) => {
   if (event.source !== window || event.origin !== window.location.origin) return;
   if (event.data?.type === "MONEY_HUB_REQUEST_CAPITAL_ONE_SYNC") {
-    chrome.runtime.sendMessage({type:"REQUEST_CAPITAL_ONE_SYNC"});
+    (typeof browser !== "undefined" ? browser : chrome).runtime.sendMessage({type:"REQUEST_CAPITAL_ONE_SYNC"});
   }
 });
